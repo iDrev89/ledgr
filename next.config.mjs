851238cc3966ba7a6fb00c1e818966effdd39e05
iDrev <jeffcepedaf@gmail.com>
2025-10-12 17:27:06 -0500

    Refactor Next.js configuration to streamline setup by reordering imports and restoring internationalization support. This update simplifies the configuration while maintaining functionality.

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// // import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
-// import createNextIntlPlugin from "next-intl/plugin";
-
-// const withNextIntl = createNextIntlPlugin();
-
-// const nextConfig = {
-//   webpack: (config, { isServer }) => {
-//     if (isServer) {
-//       config.plugins = [...config.plugins, new PrismaPlugin()];
-//     }
-
-//     return config;
-//   },
-// };
-
-// export default withNextIntl(nextConfig);
-
-import createNextIntlPlugin from "next-intl/plugin";
 /** @type {import('next').NextConfig} */
+import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig = {};
 
+const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
